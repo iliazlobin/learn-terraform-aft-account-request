dbfb4186e603edff84d8c1cecb361a66aef40c13
Ilia Zlobin <iliazlobin91@gmail.com>
2022-07-23 15:19:29 -0400

    Update sandbox aft account details

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "iac-sandbox-aft@iliazlobin.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserEmail              = "iac-sso@iliazlobin.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
